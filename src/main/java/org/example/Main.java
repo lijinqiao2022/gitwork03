e71aaeb1a246afc5d33a90227207948721e4e430
lijinqiao <lijinqiao2021@163.com>
2022-12-19 14:36:34 +0800

    第一次

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @auther:li
+ * @date:${date} - ${month} - ${day} -${time}
+ * description:${package_name}
+ * version:1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
