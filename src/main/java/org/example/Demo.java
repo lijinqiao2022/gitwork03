69f6f39018f0afaac1b1741d4b8197136aeb7adb
lijinqiao <lijinqiao2021@163.com>
2022-12-23 14:41:36 +0800

    第九次

---
+++ src/main/java/org/example/Demo.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * @auther:li
+ * @date:5 -  -  -
+ * description:
+ * version:1.0
+ */
+public class Demo {
+    public static void main(String[] args) {
+        String s=" ";
+        s=s+"a";
+        s+="bbb";
+        System.out.println(s);
+    }
+}
